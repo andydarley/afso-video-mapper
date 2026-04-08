bd22485a3dd7ba496cebaf47d461cd191966d79c
Andy Darley <andydarley@onlineability.net>
2026-04-08 21:06:31 +0100

    Removing old version

--- OLD/templates/taxonomy-county.php
+++
@@ -1,6 +0,0 @@
-<?php get_header(); $term = get_queried_object(); ?>
-<div class='county-archive'>
-<h2>AFSO Videos in <?php echo esc_html($term->name); ?></h2>
-<div class="afso-video-map" data-county-slug="<?php echo esc_attr($term->slug); ?>" style="height:600px"></div>
-</div>
-<?php get_footer(); ?>
